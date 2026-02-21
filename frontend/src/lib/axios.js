8c0bfae59ccb4feab961eda2589cea293a1d2041
chmcs2026karanmijhar-design <chmcs2026.karan.mijhar@gmail.com>
2026-02-21 12:33:26 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 const api = axios.create({
     //baseURL : 'http://localhost:3000'
-    baseURL : 'https://mern-bookstore-ylpr.onrender.com/books'
+    baseURL : 'https://mern-bookstore-ylpr.onrender.com'
 })
 
 
